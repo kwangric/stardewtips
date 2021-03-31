97ac85e81088eff82afa0dbf2a52d7938a2d61f2
Richard Kwang <richardkwang25@gmail.com>
2021-03-31 15:27:25 -0400

    Reduce number of filters. Filter cards in real time

--- fishing.js
+++ fishing.js
@@ -1,30 +1,46 @@
-let filters = [];
-
-$(":checkbox").each(function(){
-  filters.push(this.name);
-})
+// let remainingFish = []
 
 const seasons = ["spring", "summer", "fall", "winter"];
 const locations = ["ocean", "river", "mountain-lake", "pond", "desert", "ginger-island", "mines", "secret-woods", "swamp", "lair"];
 const weather = ["sun", "rain"];
 
+// Hide cards for each unchecked filter
 function cardHide(filter) {
   if ($("input[name=" + filter + "]").prop("checked") == false) {
     $("." + filter).hide();
   }
 };
 
+// Show cards for each checked filter
 function cardShow(filter) {
   if ($("input[name=" + filter + "]").prop("checked") == true) {
     $("." + filter).show();
   }
 };
 
-seasons.forEach(filter => cardHide(filter));
-seasons.forEach(filter => cardShow(filter));
+// Update cards based on filter
+function updateFish(){
+  seasons.forEach(filter => cardHide(filter));
+  seasons.forEach(filter => cardShow(filter));
+}
 
-locations.forEach(filter => cardHide(filter));
-locations.forEach(filter => cardShow(filter));
+// Create list of remaining fish
+// function defineRemainingFish() {
+//   $(".col").each(function() {
+//     if ($(this).css("display") == 'none') {}
+//   else {(remainingFish.push(this))}
+//   })
+//   remainingFish.splice(0, 4);
+// }
 
-weather.forEach(filter => cardHide(filter));
-weather.forEach(filter => cardShow(filter));
+// defineRemainingFish();
+//
+// locations.forEach(filter => cardHide(filter));
+// locations.forEach(filter => cardShow(filter));
+
+// Update cards after every checkbox toggle
+$('input:checkbox').change(
+    function(){
+        updateFish();
+        }
+    );
