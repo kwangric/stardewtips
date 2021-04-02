beb6d14a75a2da1e66cda889f134219968155ff4
Richard Kwang <richardkwang25@gmail.com>
2021-04-02 11:45:49 -0400

    Add bundle filter for fishes. Update card and button style

--- fishing.js
+++ fishing.js
@@ -1,46 +1,45 @@
-// let remainingFish = []
-
 const seasons = ["spring", "summer", "fall", "winter"];
-const locations = ["ocean", "river", "mountain-lake", "pond", "desert", "ginger-island", "mines", "secret-woods", "swamp", "lair"];
-const weather = ["sun", "rain"];
 
 // Hide cards for each unchecked filter
 function cardHide(filter) {
   if ($("input[name=" + filter + "]").prop("checked") == false) {
-    $("." + filter).hide();
+    $("." + filter).hide()
   }
 };
 
 // Show cards for each checked filter
 function cardShow(filter) {
   if ($("input[name=" + filter + "]").prop("checked") == true) {
-    $("." + filter).show();
+    $("." + filter).show()
+  }
+};
+
+// Fade cards for bundle filter
+function filterBundle() {
+  if ($("input[name='bundle']").prop("checked") == true) {
+    $(".fishes .col").not(".bundle").css("opacity", 0.25);
+  }
+  if ($("input[name='bundle']").prop("checked") == false) {
+    $(".fishes .col").not(".bundle").css("opacity", 1);
   }
 };
 
 // Update cards based on filter
-function updateFish(){
+function updateFish() {
   seasons.forEach(filter => cardHide(filter));
   seasons.forEach(filter => cardShow(filter));
 }
 
-// Create list of remaining fish
-// function defineRemainingFish() {
-//   $(".col").each(function() {
-//     if ($(this).css("display") == 'none') {}
-//   else {(remainingFish.push(this))}
-//   })
-//   remainingFish.splice(0, 4);
-// }
+// Update cards after seasonal checkbox toggle
+$('input:checkbox[name="spring"], [name="summer"], [name="fall"], [name="winter"]').change(
+  function() {
+    updateFish();
+  }
+);
 
-// defineRemainingFish();
-//
-// locations.forEach(filter => cardHide(filter));
-// locations.forEach(filter => cardShow(filter));
-
-// Update cards after every checkbox toggle
-$('input:checkbox').change(
-    function(){
-        updateFish();
-        }
-    );
+// Update cards after bundle checkbox toggle
+$('input:checkbox[name="bundle"]').change(
+  function() {
+    filterBundle();
+  }
+);
